5fdd0e5ae74ee5af42a04ef6361a8e1115425738
poppyseedcake <w.makowiec@gmail.com>
2026-03-13 23:40:09 +0100

    json

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,20 @@ import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
+import { handlerChirpsValidate } from "./api/chrips.js";
 
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
