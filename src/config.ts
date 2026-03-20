8a512714ce0958b4dbee64204ba4665b2a5537e3
poppyseedcake <w.makowiec@gmail.com>
2026-03-20 23:47:14 +0100

    drizzle config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
 type APIConfig = {
   fileServerHits: number;
+  dbURL: string;
 };
 
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+    const value = process.env[key];
+    if (!value) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
+  }
+  
 export const config: APIConfig = {
   fileServerHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
